3f40b1e81a500d4b906be166a3b441fedfe13aa6
Tiffita <74679216+Tiffita@users.noreply.github.com>
2023-09-28 16:38:18 +0200

    Add files via upload

---
+++ libft/ft_memmove.c
@@ -0,0 +1,13 @@
+#include "./libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *d = dest;
+    const unsigned char *s = src;
+    size_t i;
+
+    i = 0;
+
+    if (n == 0)
+
+}
