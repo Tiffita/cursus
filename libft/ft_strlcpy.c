da18a7f2204de7a946e7d30d70fdaf10f5864bbc
Tiffita <74679216+Tiffita@users.noreply.github.com>
2023-09-29 21:09:35 +0200

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -14,17 +14,17 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
-	while (src[i] && i + 1 < size)
+	while (src[i] != '\0' && i + 1 < size)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (i < dst)
-		dst[i] = '/0';
-	while (src[i])
+	if (size > 0)
+		dst[i] = '\0';
+	while (src[i] != '\0')
 		i++;
 	return (i);
 }
