4c5da83c1f60a7780b0c588575655516d2328f87
Tiffita <74679216+Tiffita@users.noreply.github.com>
2023-10-04 20:03:00 +0200

    Add files via upload

---
+++ libft/libft_paqueado/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prmarott <prmarott@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 18:49:35 by prmarott          #+#    #+#             */
+/*   Updated: 2023/10/04 19:44:06 by prmarott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *str, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)str)[i] == (unsigned char)c)
+			return ((unsigned char *)(str + i));
+		i++;
+	}
+	return (NULL);
+}
