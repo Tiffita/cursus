427e7aa787852bef162807565a95f2ad821b7cd1
Tiffita <74679216+Tiffita@users.noreply.github.com>
2023-10-10 17:32:26 +0200

    Update ft_memcpy.c

--- libft/libft_paqueado/ft_memcpy.c
+++ libft/libft_paqueado/ft_memcpy.c
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
 	i = 0;
+	if (!src && !dest)
+		return (NULL);
 	while (i < n)
 	{
 		d[i] = s[i];
