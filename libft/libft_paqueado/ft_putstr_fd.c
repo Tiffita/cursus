991623c9c87f3ec903611cfc0995565df507c0dc
Tiffita <74679216+Tiffita@users.noreply.github.com>
2023-10-14 06:20:20 +0200

    Add files via upload

---
+++ libft/libft_paqueado/ft_putstr_fd.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prmarott <prmarott@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 06:11:44 by prmarott          #+#    #+#             */
+/*   Updated: 2023/10/14 06:12:54 by prmarott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+/*
+oid	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
+*/
