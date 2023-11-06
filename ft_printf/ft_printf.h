c376736de50b83dfb59c045f25c7103219281ef4
Tiffita <74679216+Tiffita@users.noreply.github.com>
2023-11-06 12:56:31 +0100

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prmarott <prmarott@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 03:21:29 by prmarott          #+#    #+#             */
+/*   Updated: 2023/10/23 03:23:07 by prmarott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+// prototypes
+int	ft_printf(const char *format, ...);
+
+#endif
