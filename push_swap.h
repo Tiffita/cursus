a3ff2fa8193845703ec7890b070248809e3c06f8
Tiffita <74679216+Tiffita@users.noreply.github.com>
2024-03-26 16:09:53 +0100

    Create push_swap.h

---
+++ push_swap.h
@@ -0,0 +1,19 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include "../libft/libft.h"
+
+typedef struct s_stack
+{
+	  long			number;
+	  long			index;
+	  struct s_stack	*next;
+	  struct s_stack	*prev;
+}	  t_stack;
+
+#endif
