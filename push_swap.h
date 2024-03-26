1a61734c67a5bf799055be129eda9db93e0a7e1d
Tiffita <74679216+Tiffita@users.noreply.github.com>
2024-03-26 17:46:34 +0100

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -15,5 +15,20 @@ typedef struct Node {
     struct Node* next; // Puntero al nodo siguiente
 } Node;
 
+Node *createNode(int value, int range);
+void push(Node **top, int value, int range);
+int pop(Node **top);
+int isEmpty(Node *top);
+void freeStack(Node **top);
+void split(char *input, Node **top);
+void printOperations(int *operations, int count);
+void swap(Node **stack);
+void rotate(Node **stack);
+void reverseRotate(Node **stack);
+void pushAtoB(Node **stack_a, Node **stack_b);
+void initialPhase(Node **stack_a, int *argv, int size);
+void phase1(Node **stack_a, Node **stack_b, int size);
+void phase2(Node **stack_a, Node **stack_b);
+void phase3(Node **stack_a);
 
 #endif
