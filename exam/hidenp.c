9795fc5d3d3608fa90b4bb7c55872234624a09eb
Tiffita <74679216+Tiffita@users.noreply.github.com>
2024-04-17 11:57:21 +0200

    Update hidenp.c

--- exam/hidenp.c
+++ exam/hidenp.c
@@ -26,3 +26,30 @@ int		main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+void hidenp (char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+
+	while(s2[j])
+	{
+		if (s1[i] == s2[j])
+			i++;
+		j++;
+	}
+	if (s1[i] == '\0')
+		write(1,"1",1);
+	else 
+		write(1,"0",1);
+}
+
+int main (int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		hidenp(argv[1], argv[2]);
+	}
+	write(1, "\n", 1);
+}
